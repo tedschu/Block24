d327636a37e79f42ff0cbbf9322d3d919b81b133
tedschu <ted.schuster@gmail.com>
2024-05-07 11:46:50 -0500

    updating files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import {puppyList} from './data.js'
+
 
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(useState(puppyList))
 
   return (
     <>
       <div>
+          {
+            puppies.map((puppy) => {
+                return <p key={puppy.id}>{puppy.name}</p>
+                
 
+            })
+
+          }
 
       </div>
 
